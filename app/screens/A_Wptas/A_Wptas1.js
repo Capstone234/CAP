ebf8482043000797f83199b8b20588b97e3b7109
Angela <angela.lai237@gmail.com>
2023-08-28 13:26:16 +1000

    fix wptas page 1 styling

--- app/screens/A_Wptas/A_Wptas1.js
+++ app/screens/A_Wptas/A_Wptas1.js
@@ -3,38 +3,41 @@ import {
   Text,
   TouchableOpacity,
   View,
+  ScrollView,
+  SafeAreaView,
   ImageBackground
 } from 'react-native';
-import styles from '../../styles/A_WptasStyles/A_Wptas1Style';
-//import styles from '../../styles/Style'
-
+import uiStyle from '../../styles/uiStyle';
+import styles from '../../styles/ReactionTestsStyles/RTOneStyle';
 
 function A_Wptas1({ navigation }) {
 
   return (
 
-    <View style={styles.screen} testID="A_Wptas1_screen">
-        <View style={styles.containerText}>
-          <ImageBackground style={styles.image}
-              source = {require('../../../assets/b1.png')}>
-            <Text style={styles.text}>
-              Before proceeding, it is important to ensure that they are oriented to what is happening around them.
-              {'\n'} {'\n'}
-              On the next screen will be 5 questions. Ask the person to answer and tick the box if they give the appropriate response.
-              {'\n'} {'\n'}
-              Pay attention to their answers. Are they confused, unsure, not responding appropriately, unable to respond
-              or responding incomprehensibly? After they answer the questions it will be up to you to report the
-              manner of their response.
-            </Text>
+    <View style={uiStyle.container} testID="A_Wptas1_screen">
+        <ImageBackground style={styles.image}
+            source = {require('../../../assets/b3.png')}>
+        <ScrollView>
+            <SafeAreaView style={uiStyle.container}>
+                <Text style={uiStyle.titleText}>A-WPTAS Test</Text>
+                <Text style={uiStyle.stackedText}>
+                  Before proceeding, it is important to ensure that they are oriented to what is happening around them.
+                  {'\n'} {'\n'}
+                  On the next screen will be 5 questions. Ask the person to answer and tick the box if they give the appropriate response.
+                  {'\n'} {'\n'}
+                  Pay attention to their answers. Are they confused, unsure, not responding appropriately, unable to respond
+                  or responding incomprehensibly? After they answer the questions it will be up to you to report the
+                  manner of their response.
+                </Text>
+            </SafeAreaView>
               <TouchableOpacity
-                // onPress={() => navigation.navigate('Voms Start')}
                 onPress={() => navigation.navigate('A-WPTAS 2')}
-                style={[styles.bottomButton, styles.shadowProp]}
+                style={[styles.bottomButton, uiStyle.shadowProp]}
               >
-                <Text style={styles.buttonLabel}>I understand</Text>
+                <Text style={uiStyle.buttonLabel}>I understand</Text>
               </TouchableOpacity>
-            </ImageBackground>
-          </View>
+        </ScrollView>
+        </ImageBackground>
       </View>
 
 
