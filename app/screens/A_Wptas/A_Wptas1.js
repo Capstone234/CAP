305fe76a6a6bb031dc82332090c55e2e2262b832
Jia Zhi Ivan Jian <jjia2958@uni.sydney.edu.au>
2023-08-28 12:57:17 +1000

    moved styles

--- app/screens/A_Wptas/A_Wptas1.js
+++ app/screens/A_Wptas/A_Wptas1.js
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import {
   Text,
-  SafeAreaView,
-  StyleSheet,
   TouchableOpacity,
-  Button,
   View,
-  Alert,
-  ImageBackground,
-  Dimensions
+  ImageBackground
 } from 'react-native';
+import styles from '../../styles/A_WptasStyles/A_Wptas1Style';
 
 
 function A_Wptas1({ navigation }) {
@@ -43,91 +39,5 @@ function A_Wptas1({ navigation }) {
 
   );
 }
-
-
-// Change the detailed styles of the disclaimer page
-// Note: the general background color of this page is defined in the uiStyle
-// Elements defined in the following:
-// 1. "I understand" button
-// 2. Text position
-// 3. Text color
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#9AD3FF',
-
-  },
-  text: {
-    lineHeight: Dimensions.get('window').width/20,
-    letterSpacing: Dimensions.get('window').width/600,
-    marginHorizontal: Dimensions.get('window').width/15,
-    marginVertical: Dimensions.get('window').height/11,
-    color: '#fff',
-    fontWeight: '700',
-    fontSize: Dimensions.get('window').width/25,
-    textAlign: 'center',
-  },
-
-  bottomButton: {
-    width: Dimensions.get('window').width/1.3,
-    height: Dimensions.get('window').width/7,
-    padding: 10,
-    borderRadius: 11,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: (Dimensions.get('window').height)/1,
-    marginTop: (Dimensions.get('window').height)/300,
-    alignSelf: 'center',
-  },
-
-  buttonLabel: {
-    color: '#003A67',
-    fontSize: Dimensions.get('window').width/20,
-    fontWeight: '800',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-  },
-
-  shadowProp: {
-    shadowColor: '#171717',
-    shadowOffset: {width: -2, height: 4},
-    shadowOpacity: 0.5,
-    shadowRadius: 4,
-  },
-
-  container: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-    alignItems: 'center',
-    backgroundColor: '#349BEB',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20
-  },
-
-  containerText: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-    alignItems: 'center',
-    backgroundColor: '#349BEB',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20
-  },
-  containerBackground: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height/2,
-    alignItems: 'center',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20
-  },
-  image: {
-    width: Dimensions.get('window').width/0.99,
-    height: Dimensions.get('window').height/1.2,
-    resizeMode: 'cover',
-  },
-
-});
 
 export default A_Wptas1;
--- app/screens/A_Wptas/A_Wptas3.js
+++ app/screens/A_Wptas/A_Wptas3.js
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import {
-  StyleSheet,
   Text,
   Pressable,
-  Dimensions,
   TouchableOpacity,
   SafeAreaView,
   ScrollView
 } from 'react-native';
 import { useContext, useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
-import uiStyle from '../../components/uiStyle';
+import uiStyle from '../../styles/uiStyle';
 import cbStyle from '../../components/checkboxStyle';
+import styles from '../../styles/A_WptasStyles/A_Wptas3Style'
 
 import {
   IncidentReportRepoContext,
@@ -120,37 +119,5 @@ function A_Wptas3({ navigation }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  headingText: {
-    color: '#003A67',
-    fontWeight: 'bold',
-    fontSize: Dimensions.get('window').width/15,
-    letterSpacing: 0.3,
-    marginHorizontal: Dimensions.get('window').width/10,
-    marginVertical: Dimensions.get('window').width/20,
-    textAlign: 'center',
-  },
-  subheadingText: {
-    color: '#003A67',
-    fontWeight: 'bold',
-    fontSize: Dimensions.get('window').width/25,
-    letterSpacing: 0.3,
-    marginHorizontal: Dimensions.get('window').width/50,
-    marginVertical: Dimensions.get('window').width/250,
-    textAlign: 'center',
-  },
-  bottomButton: {
-    width: Dimensions.get('window').width/1.3,
-    height: Dimensions.get('window').width/7.5,
-    padding: 10,
-    borderRadius: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: (Dimensions.get('window').height)/25,
-    alignSelf: 'center',
-  }
-});
 
 export default A_Wptas3;
