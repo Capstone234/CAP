4f8b1d0c640d09ccbf3bb4eef72c65248ebcec94
Tisha Jhabak <iamtisha22@gmail.com>
2023-09-05 16:34:33 +1000

    adding slider styles for PCSS slider

--- app/screens/PCSSChecklist.js
+++ app/screens/PCSSChecklist.js
@@ -4,48 +4,105 @@ import {
   Text,
   TouchableOpacity,
   View,
+  StyleSheet,
   ScrollView,
+  Dimensions
 } from 'react-native';
 
-import uiStyle from '../styles/uiStyle';
+import uiStyle from '../components/uiStyle';
 import Slider from '@react-native-community/slider';
-import styles from '../styles/PCSSChecklistScreenStyle';
 import { useContext, useState} from 'react';
 import {
   IncidentReportRepoContext,
   ReportIdContext,
 } from '../components/GlobalContextProvider';
 
-import CustomSlider from './CustomSlider';
 
 
 function PCSSChecklist({ navigation }) {
+  const [sliderValues, setSliderValues] = useState({
+    headache: 0,
+    nausea: 0,
+    vomiting: 0,
+    balance: 0,
+    dizziness: 0,
+    fatigue: 0,
+    light: 0,
+    noise: 0,
+    numb: 0,
+    foggy: 0,
+    slowed: 0,
+    concentrating: 0,
+    remembering: 0,
+    drowsiness: 0,
+    sleep_less: 0,
+    sleep_more: 0,
+    sleeping: 0,
+    irritability: 0,
+    sadness: 0,
+    nervousness: 0,
+    emotional: 0,
+    blurry: 0,
+  });
 
-  const [sliderOneValue, setSliderOneValue] = useState(0);
-  const [sliderTwoValue, setSliderTwoValue] = useState(0);
-  const [sliderThreeValue, setSliderThreeValue] = useState(0);
-  const [sliderFourValue, setSliderFourValue] = useState(0);
-  const [sliderFiveValue, setSliderFiveValue] = useState(0);
-  const [sliderSixValue, setSliderSixValue] = useState(0);
-  const [sliderSevenValue, setSliderSevenValue] = useState(0);
-  const [sliderEightValue, setSliderEightValue] = useState(0);
-  const [sliderNineValue, setSliderNineValue] = useState(0);
-  const [sliderTenValue, setSliderTenValue] = useState(0);
-  const [sliderElevenValue, setSliderElevenValue] = useState(0);
-  const [sliderTwelveValue, setSliderTwelveValue] = useState(0);
-  const [sliderThirteenValue, setSliderThirteenValue] = useState(0);
-  const [sliderFourteenValue, setSliderFourteenValue] = useState(0);
-  const [sliderFifteenValue, setSliderFifteenValue] = useState(0);
-  const [sliderSixteenValue,setSliderSixteenValue] = useState(0);
-  const [sliderSeventeenValue, setSliderSeventeenValue] = useState(0);
-  const [sliderEighteenValue, setSliderEighteenValue] = useState(0);
-  const [sliderNineteenValue, setSliderNineteenValue] = useState(0);
-  const [sliderTwentyValue, setSliderTwentyValue] = useState(0);
-  const [sliderTwentyOneValue, setSliderTwentyOneValue] = useState(0);
-  const [sliderTwentyTwoValue, setSliderTwentyTwoValue] = useState(0);
-  const [sliderTwentyThreeValue, setSliderTwentyThreeValue] = useState(0);
+  const [touchPositions, setTouchPositions] = useState({
+    headache: 0,
+    nausea: 0,
+    vomiting: 0,
+    balance: 0,
+    dizziness: 0,
+    fatigue: 0,
+    light: 0,
+    noise: 0,
+    numb: 0,
+    foggy: 0,
+    slowed: 0,
+    concentrating: 0,
+    remembering: 0,
+    drowsiness: 0,
+    sleep_less: 0,
+    sleep_more: 0,
+    sleeping: 0,
+    irritability: 0,
+    sadness: 0,
+    nervousness: 0,
+    emotional: 0,
+    blurry: 0,
+  });
 
+  const handleSliderChange = (option, value) => {
+    // Update the slider value
+    setSliderValues({ ...sliderValues, [option]: value });
 
+    // Update the touch position for the Text element
+    const marginLeft = value * 50; // Adjust this factor as needed
+    setTouchPositions({ ...touchPositions, [option]: marginLeft });
+  };
+
+  const optionSliders = [
+    { label: 'Headache', key: 'headache' },
+    { label: 'Nausea', key: 'nausea' },
+    { label: 'Vomiting', key: 'vomiting' },
+    { label: 'Balance problems', key: 'balance' },
+    { label: 'Dizziness', key: 'dizziness' },
+    { label: 'Fatigue/Low energy', key: 'fatigue' },
+    { label: 'Sensitivity to light', key: 'light' },
+    { label: 'Sensitivity to noise', key: 'noise' },
+    { label: 'Numbness/Tingling', key: 'numb' },
+    { label: 'Feeling mentally foggy', key: 'foggy' },
+    { label: 'Feeling slowed down', key: 'slowed' },
+    { label: 'Difficulty concentrating', key: 'concentrating' },
+    { label: 'Difficulty remembering', key: 'remembering' },
+    { label: 'Drowsiness', key: 'drowsiness' },
+    { label: 'Sleeping less than usual', key: 'sleep_less' },
+    { label: 'Sleeping more than usual', key: 'sleep_more' },
+    { label: 'Trouble falling asleep', key: 'sleeping' },
+    { label: 'Irritability', key: 'irritability' },
+    { label: 'Sadness', key: 'sadness' },
+    { label: 'Nervousness', key: 'nervousness' },
+    { label: 'Feeling more emotional', key: 'emotional' },
+    { label: 'Blurry/Double Vision', key: 'blurry' },
+  ];
 
   return (
     <SafeAreaView style={styles.container}>
@@ -53,267 +110,83 @@ function PCSSChecklist({ navigation }) {
         <Text style={uiStyle.text}>
           Does the affected person have any of these symptoms?
         </Text>
-        <View style={[uiStyle.contentContainer]}>
-          <View style={styles.sliders}>
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Headache:</Text>
-              <Text style={[styles.text]}>{sliderOneValue}</Text>
-            </View>
-            {/* <Slider testID='headache' accessible={true} accessibilityLabel={'headache'} label='headache'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderOneValue(val)}
-              thumbStyle={styles.thumb} 
-              thumbText={sliderOneValue.toString()}
-            /> */}
-            <CustomSlider testID='headache' accessible={true} accessibilityLabel={'headache'} label='headache'
-              value={sliderOneValue}
-              onValueChange={(val) => setSliderOneValue(val)}
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-            />
 
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Nausea: </Text>
-              <Text style={[styles.text]}>{sliderTwoValue}</Text>
+        <View style={uiStyle.contentContainer}>
+            <View style={styles.sliders}>
+              {optionSliders.map((option) => (
+                <View key={option.key}>
+                  <View style={styles.sliderOne}>
+                    <Text style={[styles.text]}>{option.label}:</Text>
+                  </View>
+                  <Slider
+                    testID={option.key}
+                    accessible={true}
+                    accessibilityLabel={option.key}
+                    label={option.label}
+                    minimumValue={0}
+                    maximumValue={6}
+                    step={1}
+                    onValueChange={(val) => handleSliderChange(option.key, val)}
+                  />
+                  <Text style={{ marginLeft: touchPositions[option.key] }}>
+                    {sliderValues[option.key]}
+                  </Text>
+                </View>
+              ))}
             </View>
-            <Slider testID='nausea' accessible={true} accessibilityLabel={'nausea'} label='nausea'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderTwoValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Vomiting:</Text>
-              <Text style={[styles.text]}>{sliderThreeValue}</Text>
-            </View>
-            <Slider testID='vomiting' accessible={true} accessibilityLabel={'vomiting'} label='vomiting'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderThreeValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Balance problems:</Text>
-              <Text style={[uiStyle.text]}>{sliderFourValue}</Text>
-            </View>
-            <Slider testID='balance' accessible={true} accessibilityLabel={'balance'} label='balance'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderFourValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Dizziness:</Text>
-              <Text style={[styles.text]}>{sliderFiveValue}</Text>
-            </View>
-            <Slider testID='dizziness' accessible={true} accessibilityLabel={'dizziness'} label='dizziness'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderFiveValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Fatigue/Low energy:</Text>
-              <Text style={[uiStyle.text]}>{sliderSixValue}</Text>
-            </View>
-            <Slider testID='fatigue' accessible={true} accessibilityLabel={'fatigue'} label='fatigue'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderSixValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Sensitivity to light:</Text>
-              <Text style={[uiStyle.text]}>{sliderSevenValue}</Text>
-            </View>
-            <Slider testID='light' accessible={true} accessibilityLabel={'light'} label='light'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderSevenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Sensitivity to noise:</Text>
-              <Text style={[uiStyle.text]}>{sliderEightValue}</Text>
-            </View>
-            <Slider testID='noise' accessible={true} accessibilityLabel={'noise'} label='noise'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderEightValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Numbness/Tingling:</Text>
-              <Text style={[uiStyle.text]}>{sliderNineValue}</Text>
-            </View>
-            <Slider testID='numb' accessible={true} accessibilityLabel={'numb'} label='numb'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderNineValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Feeling mentally foggy:</Text>
-              <Text style={[uiStyle.text]}>{sliderTenValue}</Text>
-            </View>
-            <Slider testID='foggy' accessible={true} accessibilityLabel={'foggy'} label='foggy'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderTenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Feeling slowed down:</Text>
-              <Text style={[uiStyle.text]}>{sliderElevenValue}</Text>
-            </View>
-            <Slider testID='slowed' accessible={true} accessibilityLabel={'slowed'} label='slowed'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderElevenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Difficulty concentrating:</Text>
-              <Text style={[uiStyle.text]}>{sliderTwelveValue}</Text>
-            </View>
-            <Slider testID='concentrating' accessible={true} accessibilityLabel={'concentrating'} label='concentrating'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderTwelveValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Difficulty remembering:</Text>
-              <Text style={[uiStyle.text]}>{sliderThirteenValue}</Text>
-            </View>
-            <Slider testID='remembering' accessible={true} accessibilityLabel={'remembering'} label='remembering'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderThirteenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Drowsiness:</Text>
-              <Text style={[uiStyle.text]}>{sliderFourteenValue}</Text>
-            </View>
-            <Slider testID='drowsiness' accessible={true} accessibilityLabel={'drowsiness'} label='drowsiness'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderFourteenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Sleeping less than usual:</Text>
-              <Text style={[uiStyle.text]}>{sliderFifteenValue}</Text>
-            </View>
-            <Slider testID='sleep_less' accessible={true} accessibilityLabel={'sleep_less'} label='sleep_less'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderFifteenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Sleeping more than ususal:</Text>
-              <Text style={[uiStyle.text]}>{sliderSixteenValue}</Text>
-            </View>
-            <Slider testID='sleep_more' accessible={true} accessibilityLabel={'sleep_more'} label='sleep_more'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderSixteenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Trouble falling asleep:</Text>
-              <Text style={[uiStyle.text]}>{sliderSeventeenValue}</Text>
-            </View>
-            <Slider testID='sleeping' accessible={true} accessibilityLabel={'sleeping'} label='sleeping'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderSeventeenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Irritability:</Text>
-              <Text style={[uiStyle.text]}>{sliderEighteenValue}</Text>
-            </View>
-            <Slider testID='irritability' accessible={true} accessibilityLabel={'irritability'} label='irritability'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderEighteenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Sadness:</Text>
-              <Text style={[uiStyle.text]}>{sliderNineteenValue}</Text>
-            </View>
-            <Slider testID='sadness' accessible={true} accessibilityLabel={'sadness'} label='sadness'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderNineteenValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Nervousness:</Text>
-              <Text style={[uiStyle.text]}>{sliderTwentyValue}</Text>
-            </View>
-            <Slider testID='nervousness' accessible={true} accessibilityLabel={'nervousness'} label='nervousness'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderTwentyValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Feeling more emotional:</Text>
-              <Text style={[uiStyle.text]}>{sliderTwentyOneValue}</Text>
-            </View>
-            <Slider testID='emotional' accessible={true} accessibilityLabel={'emotional'} label='emotional'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderTwentyOneValue(val)}
-            />
-            <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Blurry/Double Vision:</Text>
-              <Text style={[uiStyle.text]}>{sliderTwentyTwoValue}</Text>
-            </View>
-            <Slider testID='blurry' accessible={true} accessibilityLabel={'blurry'} label='blurry'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderTwentyTwoValue(val)}
-            />
-            {/* <View style={styles.sliderOne}>
-              <Text style={uiStyle.text}>Pain other than headache:</Text>
-              <Text style={[uiStyle.text]}>{sliderTwentyThreeValue}</Text>
-            </View>
-            <Slider testID='pain' accessible={true} accessibilityLabel={'pain'} label='pain'
-              minimumValue={0}
-              maximumValue={6}
-              step={1}
-              onValueChange={(val) => setSliderTwentyThreeValue(val)}
-            /> */}
-            
           </View>
-        </View>
-        </ScrollView>
+      </ScrollView>
         <TouchableOpacity
           onPress={() => {
-            var totalSliderValue = sliderOneValue + sliderTwoValue + sliderThreeValue+sliderFourValue+sliderFiveValue+sliderSixValue+
-              sliderSevenValue+sliderEightValue+sliderNineValue+sliderTenValue+sliderElevenValue+
-              sliderTwelveValue + sliderThirteenValue+sliderFourteenValue+sliderFifteenValue+sliderSixteenValue+
-              sliderSeventeenValue+sliderEighteenValue+sliderNineteenValue+sliderTwentyValue;
-            navigation.navigate('Incident Report Result', {sliderResult: totalSliderValue});
+            const totalSliderValue = Object.values(sliderValues).reduce(
+              (acc, currentValue) => acc + currentValue,
+              0
+            );
+            navigation.navigate('Incident Report Result', {
+              sliderResult: totalSliderValue,
+            });
           }}
-          style={[styles.bottomButton, uiStyle.shadowProp]}
+          style={styles.bottomButton}
         >
-          <Text style={uiStyle.buttonLabel}>Next</Text>
+          <Text style={styles.buttonLabel}>Next</Text>
         </TouchableOpacity>
-       
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#9AD3FF'
+  },
+  sliders: {
+    width: '80%',
+  },
+
+  sliderOne: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  text: {
+    color: '#003A67',
+    fontWeight: '700',
+    fontSize: Dimensions.get('window').width/20,
+    marginHorizontal: Dimensions.get('window').width/500,
+    marginVertical: Dimensions.get('window').width/15,
+    textAlign: 'left',
+  },
+  bottomButton: {
+    width: Dimensions.get('window').width/1.3,
+    height: Dimensions.get('window').width/7.5,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: (Dimensions.get('window').height)/20,
+    alignSelf: 'center',
+  }
+});
 
 export default PCSSChecklist;
