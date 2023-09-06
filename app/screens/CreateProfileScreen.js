751f5dffccdb6d943352e2829bd5fa226658106f
Johannes So <joso4917@uni.sydney.edu.au>
2023-09-06 16:00:45 +1000

    Reformatted code in Login.js and CreateProfileScreen.js for better readability

--- app/screens/CreateProfileScreen.js
+++ app/screens/CreateProfileScreen.js
@@ -62,94 +62,92 @@ function CreateProfileScreen({ navigation }) {
   return (
     <SafeAreaView style={uiStyle.container}>
       <View style={styles.titlecontainer}>
-      <Text style={styles.text}>
-        Enter your details and the results will be saved in your profile
-      </Text>
-      <SafeAreaView style={styles.inputAreaContainer}>
-        <TextInput
-          maxLength={25}
-          style={styles.input}
-          onChangeText={onChangeFirstName}
-          value={firstNameOfUser}
-          placeholder="First Name"
-          returnKeyType="done"
-        />
-        <TextInput
-          maxLength={25}
-          style={styles.input}
-          onChangeText={onChangeLastName}
-          value={lastNameOfUser}
-          placeholder="Last Name"
-          returnKeyType="done"
-        />
-        <TextInput
-          testID='age' accessible={true} accessibilityLabel={'age'} label='age'
-          maxLength={3}
-          style={styles.input}
-          onChangeText={onChangeAge}
-          value={ageOfUser}
-          placeholder="Age"
-          keyboardType="number-pad"
-          returnKeyType="done"
-        />
-        <TextInput
-          testID='weight' accessible={true} accessibilityLabel={'weight'} label='weight'
-          maxLength={3}
-          style={styles.input}
-          onChangeText={onChangeWeight}
-          value={weightOfUser}
-          placeholder="Weight in kg"
-          keyboardType="numeric"
-          returnKeyType="done"
-        />
-        <TextInput
-          maxLength={15}
-          style={styles.input}
-          onChangeText={onChangePassword}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password (maximum 15 characters)"
-          returnKeyType="done"
-        />
-        <TouchableOpacity
-          style={[styles.bottomButton, styles.shadowProp]}
-          onPress={() => {
-
-            // Checking that none of the text fields are empty          
-            if (firstNameOfUser == ''){
-              alert('Please enter first name.');
-            } else if (lastNameOfUser == ''){
-              alert('Please enter last name.')
-            } else if (ageOfUser == ''){
-              alert('Please enter age.')
-            } else if (weightOfUser == ''){
-              alert('Please enter weight.')
-            } else if (password == ''){
-              alert('Please enter password.')
-            } else if (password.length <5){
-              alert('Please enter more than 5 characters for the password.')
-            }
-            else {
-              onCreateAccount(
-                firstNameOfUser,
-                lastNameOfUser,
-                ageOfUser,
-                weightOfUser,
-                password,
-              );
-              navigation.navigate('Home Page');
-            }
-          }}
-        >
-          <Text style={uiStyle.buttonLabel}>Submit</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.bottomButton, styles.shadowProp]}
-          onPress={() => navigation.navigate('Login')}
-        >
-          <Text style={uiStyle.buttonLabel}>Back</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
+        <Text style={styles.text}>
+          Enter your details and the results will be saved in your profile
+        </Text>
+        <SafeAreaView style={styles.inputAreaContainer}>
+          <TextInput
+            maxLength={25}
+            style={styles.input}
+            onChangeText={onChangeFirstName}
+            value={firstNameOfUser}
+            placeholder="First Name"
+            returnKeyType="done"
+          />
+          <TextInput
+            maxLength={25}
+            style={styles.input}
+            onChangeText={onChangeLastName}
+            value={lastNameOfUser}
+            placeholder="Last Name"
+            returnKeyType="done"
+          />
+          <TextInput
+            testID='age' accessible={true} accessibilityLabel={'age'} label='age'
+            maxLength={3}
+            style={styles.input}
+            onChangeText={onChangeAge}
+            value={ageOfUser}
+            placeholder="Age"
+            keyboardType="number-pad"
+            returnKeyType="done"
+          />
+          <TextInput
+            testID='weight' accessible={true} accessibilityLabel={'weight'} label='weight'
+            maxLength={3}
+            style={styles.input}
+            onChangeText={onChangeWeight}
+            value={weightOfUser}
+            placeholder="Weight in kg"
+            keyboardType="numeric"
+            returnKeyType="done"
+          />
+          <TextInput
+            maxLength={15}
+            style={styles.input}
+            onChangeText={onChangePassword}
+            value={password}
+            secureTextEntry={true}
+            placeholder="Password (maximum 15 characters)"
+            returnKeyType="done"
+          />
+          <TouchableOpacity
+            style={[styles.bottomButton, styles.shadowProp]}
+            onPress={() => {
+              // Checking that none of the text fields are empty
+              if (firstNameOfUser == '') {
+                alert('Please enter first name.');
+              } else if (lastNameOfUser == '') {
+                alert('Please enter last name.')
+              } else if (ageOfUser == '') {
+                alert('Please enter age.')
+              } else if (weightOfUser == '') {
+                alert('Please enter weight.')
+              } else if (password == '') {
+                alert('Please enter password.')
+              } else if (password.length <5) {
+                alert('Please enter more than 5 characters for the password.')
+              } else {
+                onCreateAccount(
+                  firstNameOfUser,
+                  lastNameOfUser,
+                  ageOfUser,
+                  weightOfUser,
+                  password,
+                );
+                navigation.navigate('Home Page');
+              }
+            }}
+          >
+            <Text style={uiStyle.buttonLabel}>Submit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.bottomButton, styles.shadowProp]}
+            onPress={() => navigation.navigate('Login')}
+          >
+            <Text style={uiStyle.buttonLabel}>Back</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
       </View>
     </SafeAreaView>
   );
