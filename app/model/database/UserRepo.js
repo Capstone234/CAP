945b1593336516c8fba757d786a4dffc32c622d3
Olivia <osmi3220@uni.sydney.edu.au>
2023-09-07 15:34:12 +1000

    first pass on replacing tables - currently left the singleresponse, multi etc

---
+++ app/model/database/UserRepo.js
@@ -0,0 +1,20 @@
+/**
+ * @module
+ */
+
+/**
+ * Represents a user in the database.
+ *
+ * Snake case is used for properties because it is used in the db schema.
+ */
+export class User {
+  constructor(username, fName, sName, age, weight, email, password) {
+    this.username = patientId;
+    this.fName = fName;
+    this.sName = lName;
+    this.age = age;
+    this.weight = weight;
+    this.email = email;
+    this.password = password;
+  }
+}
