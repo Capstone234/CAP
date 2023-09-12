aa2ec4a8737fbaca9248cf0445f412bffbfa6111
Johannes So <joso4917@uni.sydney.edu.au>
2023-09-12 12:11:57 +1000

    Configure .detoxrc.js to align with Expo's guide

--- .detoxrc.js
+++ .detoxrc.js
@@ -1,48 +1,57 @@
 /** @type {Detox.DetoxConfig} */
 module.exports = {
+  logger: {
+    level: process.env.CI ? 'debug' : undefined,
+  },
   testRunner: {
+    $0: 'jest',
     args: {
-      '$0': 'jest',
-      config: 'e2e/jest.config.js'
+      config: 'e2e/jest.config.js',
+      _: ['e2e'],
     },
-    jest: {
-      setupTimeout: 150000
-    }
+  },
+  artifacts: {
+    plugins: {
+      log: process.env.CI ? 'failing' : undefined,
+      screenshot: 'failing',
+    },
   },
   apps: {
-    'ios': {
+    'ios.release': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/YOUR_APP.app',
-      build: 'xcodebuild -workspace ios/YOUR_APP.xcworkspace -scheme YOUR_APP -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+      build:
+        'xcodebuild -workspace ios/ConcussionActionPlan.xcodeproj -scheme ConcussionActionPlan -configuration Release -sdk iphonesimulator -arch x86_64 -derivedDataPath ios/build',
+      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/ConcussionActionPlan.app',
     },
-    'android': {
+    'android.release': {
       type: 'android.apk',
+      build:
+        'cd android && ./gradlew :app:assembleRelease :app:assembleAndroidTest -DtestBuildType=release && cd ..',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
-    }
+    },
   },
   devices: {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 12 Pro'
-      }
+        type: 'iPhone 14',
+      },
     },
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_4a_API_31'
-      }
-    }
+        avdName: 'pixel_4',
+      },
+    },
   },
   configurations: {
-    'ios': {
+    'ios.release': {
       device: 'simulator',
-      app: 'ios'
+      app: 'ios.release',
     },
-    'android': {
+    'android.release': {
       device: 'emulator',
-      app: 'android'
-    }
-  }
+      app: 'android.release',
+    },
+  },
 };
