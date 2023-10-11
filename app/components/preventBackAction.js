20d66a97cb1d19bdf2a5064090275e2fa8a52876
Olivia <osmi3220@uni.sydney.edu.au>
2023-10-11 14:30:31 +1100

    Created function to prevent using back on any screen and implemented on red flag test

---
+++ app/components/preventBackAction.js
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { BackHandler, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+// code to prevent users from going back mid test. already cannot resubmit result
+// on most tests anyway but this will prevent it on the few that can, and also
+// prevent non-unique key warnings.
+
+const preventBackAction = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Error", "Cannot go back during testing.", [
+        {
+          text: "OK",
+          onPress: () => null,
+          style: "cancel"
+        }
+      ]);
+      return true; // This will prevent the default back action
+    };
+
+    const addBackListener = () => {
+      BackHandler.addEventListener("hardwareBackPress", backAction);
+    };
+
+    const removeBackListener = () => {
+      BackHandler.removeEventListener("hardwareBackPress", backAction);
+    };
+
+    // Add the listener when the screen is focused
+    const focusSubscription = navigation.addListener('focus', addBackListener);
+    // Remove the listener when the screen loses focus
+    const blurSubscription = navigation.addListener('blur', removeBackListener);
+
+    return () => {
+      // Clean up event listeners upon unmount or when navigating away
+      focusSubscription && focusSubscription();
+      blurSubscription && blurSubscription();
+      removeBackListener();
+    };
+  }, [navigation]);
+};
+
+export default preventBackAction;
