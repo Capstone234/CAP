e0a0e1b853c34c73ae09d1a77a701af5be36a17b
Tina Ngo <tngo9001@uni.sydney.edu.au>
2023-10-12 20:48:32 +1100

    Fig broken things in TestListScreen after merging

--- app/screens/TestListScreen.js
+++ app/screens/TestListScreen.js
@@ -1,26 +1,67 @@
 import * as React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  Pressable,
+  TouchableOpacity,
+  SafeAreaView,
+  View,
+  ScrollView
+} from 'react-native';
 import styles from '../styles/DisclaimerStyle';
+import { Ionicons } from '@expo/vector-icons';
+import uiStyle from '../styles/uiStyle';
 
+import {
+  //Import the code we need. eg this now uses MedicalReportRepoContext
+  //so we got to import that for the DB functionality.
+  IncidentReportRepoContext,
+  IncidentIdContext,
+  UserContext
+} from '../components/GlobalContextProvider';
 
 function TestsListScreen({ navigation, route }) {
-  const { tests } = route.params;
+  const { incidentId, updateIncidentId } = useContext(IncidentIdContext);
+  const incidentReportRepoContext = useContext(IncidentReportRepoContext);
+
+
+//  const { tests } = route.params;
+  const handleText1Click = () => {
+    // Handle click for Text 1
+    console.log('Text 1 clicked');
+  };
+
+  const handleText2Click = () => {
+    // Handle click for Text 2
+    console.log('Text 2 clicked');
+  };
 
   return (
-    <View >
-      <Text>List of Available Tests:</Text>
-      {tests.map((test, index) => (
-        <TouchableOpacity
-          key={index}
-          onPress={() => {
-            // Navigate to the selected test screen when a test is clicked
-            navigation.navigate(test.screen);
-          }}
-        >
-          <Text>{test.title}</Text>
+    console.log(incidentId);
+    console.log(incidentReportRepoContext);
+
+    <View>
+        <TouchableOpacity onPress={handleText1Click}>
+          <Text>Text 1</Text>
         </TouchableOpacity>
-      ))}
+
+        <TouchableOpacity onPress={handleText2Click}>
+          <Text>Text 2</Text>
+        </TouchableOpacity>
     </View>
+//    <View >
+//      <Text>List of Available Tests:</Text>
+//      {tests.map((test, index) => (
+//        <TouchableOpacity
+//          key={index}
+//          onPress={() => {
+//            // Navigate to the selected test screen when a test is clicked
+//            navigation.navigate(test.screen);
+//          }}
+//        >
+//          <Text>{test.title}</Text>
+//        </TouchableOpacity>
+//      ))}
+//    </View>
 
   );
 }
